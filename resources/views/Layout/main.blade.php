6365e6f3d3d7507a114a9cd8eecccad231904a9a
Muticlone <marlussantos2010hotmail.com@gmail.com>
2023-07-13 13:47:14 -0300

    mudaças no navbar

--- resources/views/Layout/main.blade.php
+++ resources/views/Layout/main.blade.php
@@ -22,145 +22,37 @@
     <body class="container">
     <header>
        
-        {{-- <nav class="navbar navbar-expand-lg bg-light-tertiary" id="navbar">
-            <div class="container-fluid">
-                 <a href="{{route('home');}}">
-                    <img src="/img/logo.png" alt="logo">
-                </a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse " id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{route('home');}}">Home</a>
-                  </li>
-                  
-                  @auth
-                
-                   
-                 
+       
 
-                  
-                      
-                  
-
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Empresa
-                    </a>
-                    <ul class="dropdown-menu">
-
-                        
-                       
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('pag.cadastrar.Empresa');}}">Novo Cadastro Empresa</a>
-                        </li>
-                        
-                        <li class="nav-item">
-                            <a class="nav-link" href="/dashboard">Minhas empresas</a>
-                        </li>
-        
-                         
-                        
-        
-                        <li class="nav-item">
-                    </ul>
-                  </li>
-                 
-                  @endauth
-                  
-                  @guest
-                      
-                  <li class="nav-item">
-                    <a class="nav-link" href="/login">Entrar</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="/register">Cadastrar</a>
-                  </li>
-                  
-                  @endguest
-
-                 
-                </ul>
-                <ul class="navbar-nav second-ul">
-                  
-                    <li class="nav-item dropdown" id ='info-container'>
-                        <a class="nav-link dropdown-toggle current-theme tema" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-sliders" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M11.5 2a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM9.05 3a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0V3h9.05zM4.5 7a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM2.05 8a2.5 2.5 0 0 1 4.9 0H16v1H6.95a2.5 2.5 0 0 1-4.9 0H0V8h2.05zm9.45 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-2.45 1a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0v-1h9.05z"/>
-                          </svg> 
-                        </a>
-
-                        
-                       
-                        <ul class="dropdown-menu themes-list">
-                          @auth 
-                          <li>
-                            <a class="dropdown-item" href="/user/profile">
-                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                              </svg> Meu perfil
-                            </a>
-                          </li>
-                          @endauth
-                          <ul class="dropdown-menu themes-list"> 
-                            
-                          </ul>
-                          <li>
-                              <samp>Temas</samp>
-                                <a class="dropdown-item" href="#" data-theme="light">
-                                  Claro <ion-icon name="sunny-outline"></ion-icon> 
-                                </a>
-                            </li>
-                            <li>
-                                <a class="dropdown-item" href="#" data-theme="dark">
-                                  Escuro <ion-icon name="moon-outline"></ion-icon> 
-                                </a>
-                            </li>
-                            
-                            @auth 
-                            <li class="nav-item">
-                              <form action="/logout" method="POST">
-                                  @csrf
-                                      <a class="nav-link" href="/logout"
-                                          onclick="event.preventDefault(); this.closest('form').submit();">
-                                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
-                                            <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
-                                          </svg>
-                                          Sair</a>
-                              </form>
-                          </li>   
-                            @endauth
-                            
-                        </ul>
-                    </li>
-                </ul>
-              
-              </div>
-            </div>
-         
-        </nav> --}}
-
-        <nav class="navbar navbar-expand-lg bg-light-tertiary">
+      <nav class="navbar navbar-expand-lg bg-light-tertiary">
           <div class="container-fluid">
+            
             <a href="{{route('home');}}">
+              
               <img class="imgnavbar" src="/img/logo.png" alt="logo">
-          </a>
+            </a>
+            
             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+
+            <div class="offcanvas offcanvas-start " tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
               <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                <a href="{{route('home');}}">
+              
+                  <img class="imgnavbar" src="/img/logo.png" alt="logo">
+                </a>
+                
                 <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
               </div>
               <div class="offcanvas-body">
                 <ul class="navbar-nav">
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                    
+                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">
+                      
+                      Home
+                    </a>
                   </li>
                   @auth
                   <li class="nav-item dropdown">
@@ -168,14 +60,16 @@
                       Empresa
                     </a>
                     <ul class="dropdown-menu">
-                      <li class="nav-item">
-                        <a class="nav-link" href="{{ route('pag.cadastrar.Empresa') }}">Novo Cadastro Empresa</a>
+                      <li class="nav-item linha">
+                        <a class="nav-link tamnhofonte" href="{{ route('pag.cadastrar.Empresa') }}">Novo Cadastro</a>
                       </li>
-                      <li class="nav-item">
+                      
+                      <li class="nav-item tamnhofonte">
                         <a class="nav-link" href="/dashboard">Minhas empresas</a>
                       </li>
                     </ul>
                   </li>
+                 
                   @endauth
                   @guest
                   <li class="nav-item">
@@ -185,6 +79,7 @@
                     <a class="nav-link" href="/register">Cadastrar</a>
                   </li>
                   @endguest
+                  @auth 
                   <ul class="navbar-nav second-ul">
                   
                     <li class="nav-item dropdown" id ='info-container'>
@@ -192,26 +87,37 @@
                           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-sliders" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M11.5 2a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM9.05 3a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0V3h9.05zM4.5 7a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM2.05 8a2.5 2.5 0 0 1 4.9 0H16v1H6.95a2.5 2.5 0 0 1-4.9 0H0V8h2.05zm9.45 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-2.45 1a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0v-1h9.05z"/>
                           </svg> 
+                          Ajustes
                         </a>
 
                         
                        
                         <ul class="dropdown-menu themes-list">
-                          @auth 
-                          <li>
-                            <a class="dropdown-item" href="/user/profile">
+                          
+                          <li >
+                            <a class="dropdown-item linha" href="/user/profile">
                               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                               </svg> Meu perfil
                             </a>
                           </li>
-                          @endauth
-                          <ul class="dropdown-menu themes-list"> 
-                            
-                          </ul>
+                          
+                          
                           <li>
-                              <samp>Temas</samp>
+                            <a class="dropdown-item ">
+                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lamp-fill" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M5.04.303A.5.5 0 0 1 5.5 0h5c.2 0 .38.12.46.303l3 7a.5.5 0 0 1-.363.687h-.002c-.15.03-.3.056-.45.081a32.731 32.731 0 0 1-4.645.425V13.5a.5.5 0 1 1-1 0V8.495a32.753 32.753 0 0 1-4.645-.425c-.15-.025-.3-.05-.45-.08h-.003a.5.5 0 0 1-.362-.688l3-7Z"/>
+                                <path d="M6.493 12.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.052.075l-.001.004-.004.01V14l.002.008a.147.147 0 0 0 .016.033.62.62 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.62.62 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465-1.281 0-2.462-.172-3.34-.465-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411Z"/>
+                              </svg>
+                              Tema
+                            </a>
+                            
+                          </li>
+                          
+                            <li>
+                             
+
                                 <a class="dropdown-item" href="#" data-theme="light">
                                   Claro <ion-icon name="sunny-outline"></ion-icon> 
                                 </a>
@@ -221,30 +127,35 @@
                                   Escuro <ion-icon name="moon-outline"></ion-icon> 
                                 </a>
                             </li>
-                            
-                            @auth 
-                            <li class="nav-item">
+                            <div class="linha"></div>
+                            <li >
                               <form action="/logout" method="POST">
-                                  @csrf
-                                      <a class="nav-link" href="/logout"
-                                          onclick="event.preventDefault(); this.closest('form').submit();">
-                                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
-                                            <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
-                                          </svg>
-                                          Sair</a>
+                                <a class="dropdown-item" href="/logout"
+                                onclick="event.preventDefault(); this.closest('form').submit();">
+                                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+                                    <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+                                  </svg> Sair
+                                </a>
                               </form>
-                          </li>   
-                            @endauth
+                            </li>
                             
-                        </ul>
+                           
+                            
+                        
+                        @endauth
                     </li>
-                </ul>
+                    
+                  </ul>
+                  <li  style='display: none;' class="nav-item">
+                    
+                  </li>
+                
                 </ul>
               </div>
             </div>
           </div>
-        </nav>
+      </nav>
         
 
        
