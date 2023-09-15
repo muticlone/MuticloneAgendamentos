ef77fd9b85612623125802be97752c3c1b07bcac
Muticlone <marlussantos2010hotmail.com@gmail.com>
2023-09-15 19:02:44 -0300

    css

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
+
     plugins: [
         laravel({
             input: [
