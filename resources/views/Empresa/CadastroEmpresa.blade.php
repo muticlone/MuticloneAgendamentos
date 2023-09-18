53042dad3b0f83e5d0380ae729ce4386989d0dc2
Muticlone <marlussantos2010hotmail.com@gmail.com>
2023-09-18 15:58:44 -0300

    melhoria mascaras

--- resources/views/Empresa/CadastroEmpresa.blade.php
+++ resources/views/Empresa/CadastroEmpresa.blade.php
@@ -223,8 +223,7 @@
                                 </svg>
                             </span>
                             <input type="text" class="form-control" id="cep" name="cep" placeholder="Cep"
-                                onblur="pesquisacep(this.value);" OnKeyPress="formatar('#####-###', this)" size="10"
-                                maxlength="9" aria-describedby="validationTooltipUsernamePrepend" required />
+                                onblur="pesquisacep(this.value);"  aria-describedby="validationTooltipUsernamePrepend" required />
                             <div class="invalid-tooltip">
                                 Por favor, digite seu número do seu cep
                             </div>
@@ -355,7 +354,7 @@
                                         d="M4 4a4 4 0 1 1 4.5 3.969V13.5a.5.5 0 0 1-1 0V7.97A4 4 0 0 1 4 3.999zm2.493 8.574a.5.5 0 0 1-.411.575c-.712.118-1.28.295-1.655.493a1.319 1.319 0 0 0-.37.265.301.301 0 0 0-.057.09V14l.002.008a.147.147 0 0 0 .016.033.617.617 0 0 0 .145.15c.165.13.435.27.813.395.751.25 1.82.414 3.024.414s2.273-.163 3.024-.414c.378-.126.648-.265.813-.395a.619.619 0 0 0 .146-.15.148.148 0 0 0 .015-.033L12 14v-.004a.301.301 0 0 0-.057-.09 1.318 1.318 0 0 0-.37-.264c-.376-.198-.943-.375-1.655-.493a.5.5 0 1 1 .164-.986c.77.127 1.452.328 1.957.594C12.5 13 13 13.4 13 14c0 .426-.26.752-.544.977-.29.228-.68.413-1.116.558-.878.293-2.059.465-3.34.465-1.281 0-2.462-.172-3.34-.465-.436-.145-.826-.33-1.116-.558C3.26 14.752 3 14.426 3 14c0-.599.5-1 .961-1.243.505-.266 1.187-.467 1.957-.594a.5.5 0 0 1 .575.411z" />
                                 </svg>
                             </span>
-                            <input type="text" class="form-control" id="uf" maxlength="2" name="uf"
+                            <input type="text" class="form-control" id="uf" name="uf"
                                 placeholder="Uf" required aria-describedby="validationTooltipUsernamePrepend" required />
                             <div class="invalid-tooltip">
                                 Por favor, digite a sigla do seu estado, por exemplo, 'BA'.
@@ -423,14 +422,14 @@
 
                             </div>
                         </div>
-                        <button type="button" class="btn btn-success" id="generateDescription" data-bs-toggle="tooltip"
+                        {{-- <button type="button" class="btn btn-success" id="generateDescription" data-bs-toggle="tooltip"
                             data-bs-placement="bottom" data-bs-custom-class="custom-tooltip"
                             data-bs-title="Escolha uma área de Atuação é digite nome fantasia, depois gere aqui uma descrição automática para o seu negócio">
                             <span id="nomeDesci" style="margin-right: 10px;">Gere uma descrição</span>
                             <span id="loadingMessage" style="display: none;" class="spinner-border spinner-border-sm"
                                 role="status" aria-hidden="true"></span>
 
-                        </button>
+                        </button> --}}
 
 
 
@@ -690,6 +689,23 @@
     <script>$('#Celular').inputmask("(99) 99999-9999");</script>
     <script>$('#telefone').inputmask("(99) 9999-9999");</script>
     <script>$('#cnpj').inputmask("99.999.999/9999-99");</script>
+    <script>$('#cep').inputmask("99999-999");</script>
+    <script>
+        $('#numero_endereco').inputmask({
+          mask: "99999999",
+          placeholder: ""
+        });
 
+    </script>
+    <script>
+         $('#uf').inputmask({
+          mask: "AA",
+          placeholder: ""
+        });
+
+    </script>
+
+
+    </style>
     <script src="/js/Tooltips.js"></script>
 @endsection
