cfe813a4834d983f737ee593f6cac71744d9c541
Muticlone <marlussantos2010hotmail.com@gmail.com>
2023-09-18 15:20:12 -0300

    css 15:20 18-09-2023

--- resources/views/components/menu_agendamentos.blade.php
+++ resources/views/components/menu_agendamentos.blade.php
@@ -4,11 +4,11 @@
     <div class="col-lg-12 col-sm-12 col-md-12 pt-2" align="center">
         <div class="btn-group" role="group" aria-label="Basic example">
 
-            <a href="{{ route('meus.clientes.agendamentos.empresa', ['id' => $empresa_id, 'status' => 'ativos']) }}" class="btn btn-sm btn-outline-warning btndashboardservico">Ativos</a>
-            <a href="{{ route('meus.clientes.agendamentos.empresa', ['id' => $empresa_id, 'status' => 'pendentes']) }}" class="btn btn-sm btn-outline-warning btndashboardservico">Pendentes</a>
-            <a href="{{ route('meus.clientes.agendamentos.empresa', ['id' => $empresa_id, 'status' => 'confirmados']) }}" class="btn btn-sm btn-outline-info btndashboardservico">Confirmados</a>
-            <a href="{{ route('meus.clientes.agendamentos.empresa', ['id' => $empresa_id, 'status' => 'finalizados']) }}" class="btn btn-sm btn-outline-success btndashboardservico">Finalizados</a>
-            <a href="{{ route('meus.clientes.agendamentos.empresa', ['id' => $empresa_id, 'status' => 'cancelados']) }}" class="btn btn-sm btn-outline-danger btndashboardservico">Cancelados</a>
+            <a href="{{ route('meus.clientes.agendamentos.empresa', ['id' => $empresa_id, 'status' => 'ativos']) }}" class="btn btn-sm btn-outline-warning btndashboardservico btn-extra-sm">Ativos</a>
+            <a href="{{ route('meus.clientes.agendamentos.empresa', ['id' => $empresa_id, 'status' => 'pendentes']) }}" class="btn btn-sm btn-outline-warning btndashboardservico btn-extra-sm">Pendentes</a>
+            <a href="{{ route('meus.clientes.agendamentos.empresa', ['id' => $empresa_id, 'status' => 'confirmados']) }}" class="btn btn-sm btn-outline-info btndashboardservico btn-extra-sm">Confirmados</a>
+            <a href="{{ route('meus.clientes.agendamentos.empresa', ['id' => $empresa_id, 'status' => 'finalizados']) }}" class="btn btn-sm btn-outline-success btndashboardservico btn-extra-sm">Finalizados</a>
+            <a href="{{ route('meus.clientes.agendamentos.empresa', ['id' => $empresa_id, 'status' => 'cancelados']) }}" class="btn btn-sm btn-outline-danger btndashboardservico btn-extra-sm">Cancelados</a>
 
 
 
