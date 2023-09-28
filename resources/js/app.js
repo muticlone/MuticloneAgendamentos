5494d3b70b7aaac341294d6266955a5cb673e4c4
Muticlone <marlussantos2010hotmail.com@gmail.com>
2023-09-28 16:51:33 -0300

    teste3

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,10 +12,28 @@ Alpine.start();
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').then((registration) => {
-        console.log('Service Worker registrado com sucesso:', registration);
-      }).catch((error) => {
-        console.log('Erro ao registrar Service Worker:', error);
-      });
+        navigator.serviceWorker.register('/sw.js').then((registration) => {
+            console.log('Service Worker registrado com sucesso:', registration);
+        }).catch((error) => {
+            console.log('Erro ao registrar Service Worker:', error);
+        });
     });
-  }
+}
+
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+const privateKeyPath = 'key.pem'; // Caminho para key.pem
+const certificatePath = 'cert.pem'; // Caminho para cert.pem
+
+https.createServer({
+    key: fs.readFileSync(privateKeyPath),
+    cert: fs.readFileSync(certificatePath)
+}, app)
+    .listen(3000, () => {
+        console.log('Aplicativo rodando na https://seu_endereco:3000/');
+    });
+
